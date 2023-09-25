5af4d811c045c9fce1397d6d4074d334dd5cad66
Ares <diego.amachiflores@gmail.com>
2023-09-25 10:15:04 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NavBar from "./header/NavBar";
+import Clientes from "./pages/Clientes/Clientes";
+import EstudioContable from "./pages/estudio-contable/EstudioContable";
+import Inicio from "./pages/inicio/Inicio";
+import Servicios from "./pages/servicios/Servicios";
+
+function App() {
+
+  return (
+    <div>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route exact path='/' element={ <Inicio/>} />
+          <Route path='/Clientes' element={ <Clientes/>}/>
+          <Route path='/Estudio-Contable' element={ <EstudioContable/>}/>
+          <Route path='/Servicios' element={ <Servicios/>}/>
+        </Routes>
+      </Router>
+    </div>
+    )
+}
+
+export default App
+ 
---
+++ src/pages/inicio/Inicio.jsx
@@ -0,0 +1,57 @@
+const features = [
+    { name: 'Origin', description: 'Designed by Good Goods, Inc.' },
+    { name: 'Material', description: 'Solid walnut base with rare earth magnets and powder coated steel card cover' },
+    { name: 'Dimensions', description: '6.25" x 3.55" x 1.15"' },
+    { name: 'Finish', description: 'Hand sanded and finished with natural oil' },
+    { name: 'Includes', description: 'Wood card tray and 3 refill packs' },
+    { name: 'Considerations', description: 'Made from natural materials. Grain and color vary with each item.' },
+  ]
+  
+  export default function Inicio() {
+    return (
+      <div className="bg-black">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+          <div>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">RVC Servicios Contables</h2>
+            <p className="mt-4 text-gray-500">
+            Les damos la más cordial y sincera bienvenida a nuestra página web, en la que, esperamos, encontrarán toda la información que necesiten acerca de nosotros. El objetivo principal del Estudio Contable RVC Servicios Contables es brindar una asesoría correcta y profesional a nuestros clientes en las áreas de Contabilidad, Tributación, Asesoría Laboral, Financiera y Administrativa.
+            En esta página web, también podrá interactuar con nosotros, contactarnos, hacer preguntas, enviar sugerencias y comentarios relacionados con nuestros servicios y nuestra empresa, asegurando poder atenderlo y responderle oportunamente.
+            Bienvenido a nuestra página.
+            </p>
+  
+            <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+              {features.map((feature) => (
+                <div key={feature.name} className="border-t border-gray-200 pt-4">
+                  <dt className="font-medium text-gray-900">{feature.name}</dt>
+                  <dd className="mt-2 text-sm text-gray-500">{feature.description}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+          <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
+              alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
+              alt="Top down view of walnut card tray with embedded magnets and card groove."
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
+              alt="Side of walnut card tray with card groove and recessed card area."
+              className="rounded-lg bg-gray-100"
+            />
+            <img
+              src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
+              alt="Walnut card tray filled with cards and card angled in dedicated groove."
+              className="rounded-lg bg-gray-100"
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
